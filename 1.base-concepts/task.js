5a739a6f7eb5ec03fb5f6c823e2cca2cc2d82ded
Сем Елена <semova.s81@bk.ru>
2022-11-27 17:33:01 +0300

    task 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,23 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = ( Math.pow(b, 2) - 4*a*c );
+  if ( d < 0) {
+    arr.push["Корней нет"];
+  } else {
+    if ( d = 0 ) {
+      let x = (-b + Math.sqrt(d) )/(2*a);
+      arr[0] = x;
+    } else {
+      if ( d > 0 ) {
+        x = (-b + Math.sqrt(d) )/(2*a);
+        arr[0] = x;
+        let xx = (-b - Math.sqrt(d) )/(2*a);
+        arr[1] = xx;
+      }
+    }
+  }
+  console.log[arr];
   return arr;
 }
 
