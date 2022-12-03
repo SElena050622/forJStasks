4f2fa4d66a0fe4797acae4ad55e1b4ddf04f649a
Сем Елена <semova.s81@bk.ru>
2022-12-03 21:27:21 +0300

    t1 good, t2 more

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,25 +2,32 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = ( Math.pow(b, 2) - 4*a*c );
-  if ( d < 0) {
-    arr.push["Корней нет"];
-  } else {
-    if ( d = 0 ) {
-      let x = (-b + Math.sqrt(d) )/(2*a);
-      arr[0] = x;
-    } else {
-      if ( d > 0 ) {
-        x = (-b + Math.sqrt(d) )/(2*a);
-        arr[0] = x;
-        let xx = (-b - Math.sqrt(d) )/(2*a);
-        arr[1] = xx;
-      }
+  console.log(d);
+  
+  if ( d > 0 ) {
+    let x = (-b + Math.sqrt(d) )/(2*a);        
+    let xx = (-b - Math.sqrt(d) )/(2*a);
+    arr = [x,xx];
+  } else if ( d === 0 ) {
+    let x = (-b )/(2*a);
+    arr = [x];      
     }
-  }
-  console.log[arr];
+  
   return arr;
+  console.log(arr[x,xx]);
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  console.log(Number.isNaN(NaN));       // true
+
+  let P = (percent / 100 / 12);  // 1/12 процентной ставки от 0 до 1
+  let S = (amount - contribution);  // тело кредита, из общего кредита вычитаем исходную сумму
+  let amountMonths = (S * (P + (P / ( Math.pow((1 + P), countMonths) - 1)))); // Ежемесячная оплата
+  let amountAll =  (amountMonths * countMonths);  //  суммa, которую в итоге заплатит клиент (первоначальный взнос, погашение основного долга, проценты за пользование кредитом)
+  return amountAll;
+  amountAll = Math.ceil(2);  //  округление до 2 значений  после запятой
+  // console.log(amountAll);
+  return amountAll;
 }
+
+
